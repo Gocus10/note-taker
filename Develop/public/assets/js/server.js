463118daeab66236da2b7821691c7fd00b228219
Gocus10 <37048598+Gocus10@users.noreply.github.com>
2020-12-12 14:44:37 -0600

    fixed port for heroku

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const data = require('../../../db/db.json');
+const { data } = require('../../../db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, '../../notes.html'));
   });
   
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../../index.html'));
   });
   
 app.get('/api/notes', (req, res) => {
